384d3e530031ba46e1ee4b46bd011767b9824bd1
devil-k <ehdgur0812@gmail.com>
2023-07-31 21:05:09 +0900

    add jansson

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include "jansson/jansson.h"
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello, World2!\n");
+
+    json_t* sJsonRoot;
+    sJsonRoot = json_object();
+
+    json_t* sJsonObj;
+    sJsonObj = json_integer(10);
+
+    json_object_set_new(sJsonRoot, "test", sJsonObj);
+
+    printf("Json Test[%s]", json_dumps(sJsonRoot, JSON_ENCODE_ANY));
+
     return 0;
 }
